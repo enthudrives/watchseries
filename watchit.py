ee1f7182ec4f4d1f977178f5a9bdd95b1945d450
Suganthi <suganthikrishnavathi@gmail.com>
2017-11-03 10:09:32 +0000

    Update regex

--- watchit.py
+++ watchit.py
@@ -4,14 +4,14 @@ import sys
 
 class WatchSeries(scrapy.Spider):
     name = 'watchseries'
-    series = 'the_series_name'
+    series = 'the_series'
     season = '1'
     episode = '1'
     start_urls = ['http://ewatchseries.to/episode/'+ series +'_' + 's' + season + '_e' + episode + '.html']
 
     def parse(self, response):
         for title in response.xpath("//a[contains(@onclick, '.me/')]").extract():
-            yield {'url': re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", title)[0]}
+            yield {'url': re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))[^\']+", title)[0]}
 
         for next_page in response.css('div.prev-post > a'):
             yield response.follow(next_page, self.parse)
